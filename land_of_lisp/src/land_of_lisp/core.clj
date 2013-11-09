7c2ef654c8160b14b2f1edf63368bc0ead306f49
Madan Kumar Rajan <madan@seattlehomepro.com>
2013-11-09 13:43:42 +0530

    Adding basic sort algorithms

--- land_of_lisp/src/land_of_lisp/core.clj
+++ land_of_lisp/src/land_of_lisp/core.clj
@@ -1,5 +1,12 @@
 (ns land-of-lisp.core)
 
+(defn average [& xs]
+  (/ (apply + xs)
+     (count xs)))
+
+(defn next-guess [low-limit up-limit]
+  (average low-limit up-limit))
+
 (defn foo
   "I don't do a whole lot."
   [x]
